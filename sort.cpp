9f5dae0d8f4b226a14ef19f495ea65e784924764
sK0pe <pradyumn.vij@gmail.com>
2016-08-06 00:55:07 +0800

    Finished heapsort Need to do Radix Sort

--- sort.cpp
+++ sort.cpp
@@ -5,15 +5,17 @@
 #include <iomanip>
 using namespace std;
 
+
+//	Bubble Sort ----------------------------------------------------------------
 void bubbleSort(vector<int>& container){
 	int length = container.size();
 	bool swapped = false;
 	for(int i = 0; i < length; ++i){
 		swapped = false;
-		for(int j = 1; j < length; ++j){
-			if(container[j] < container[j-1]){
-				//cout << "j is " << container[j] << "j+1 is " << container[j+1] << endl;
-				swap(container[j], container[j-1]);
+		for(int endIndex = 1; endIndex < length; ++endIndex){
+			if(container[endIndex] < container[endIndex-1]){
+				//cout << "endIndex is " << container[endIndex] << "endIndex+1 is " << container[endIndex+1] << endl;
+				swap(container[endIndex], container[endIndex-1]);
 				swapped = true;
 			}
 		}
@@ -23,27 +25,39 @@ void bubbleSort(vector<int>& container){
 	}
 }
 
+//	Insertion Sort--------------------------------------------------------------
 void insertSort(vector<int>& container){
 	int key, i;
 	int length = container.size();
-	for(int j = 1; j < length; ++j){
-		key = container[j];
-		i = j - 1;
+	// start 1 ahead
+	for(int endIndex = 1; endIndex < length; ++endIndex){
+		// key or pivot is at outer loop index, save in key memory
+		key = container[endIndex];
+		// inner index loop is 1 behind initially
+		i = endIndex - 1;
+		// while inner indices still exist and associated values are greater than
+		// the key
 		while(i >= 0 && container[i] > key){
+			// move all values up if greater than the key value, that are less than endIndex
 			container[i+1] = container[i];
 			--i;
 		}
+		// place key at position where it is known to be larger than all before it
+		// and less than all after it
 		container[i+1] = key;
 	}
 }
 
+//	Merge Sort -----------------------------------------------------------------
 void merge(vector<int>& container, vector<int>& helper, int begin, int mid, int end){
 	for(int i = begin; i <= end; ++i){
 		helper[i] = container[i];
 	}
 
+	// Pointers working on helper copy of container
 	int helpLeft = begin;
 	int helpRight = mid + 1;
+	// Receiving pointer working on origianl memory of container
 	int current = begin;
 
 	while(helpLeft <= mid && helpRight <= end){
@@ -63,14 +77,19 @@ void merge(vector<int>& container, vector<int>& helper, int begin, int mid, int 
 }
 
 void mergeSort(vector<int>& container, vector<int>& helper, int left, int right){
+	// stop recursion only when split into single element containers
 	if(left < right){
 		int mid = (left + right) / 2;
+		// sort left at each level of recursion
 		mergeSort(container, helper, left, mid);
+		// sort right at each level of recursion
 		mergeSort(container, helper, mid+1, right);
+		// merge at each level 
 		merge(container, helper, left, mid, right);
 	}	
 }
 
+// Quick Sort-------------------------------------------------------------------
 int partition(vector<int>& container, int left, int right){
 	int pivot = container[(left + right)/2];
 	while(left <= right){
@@ -80,7 +99,7 @@ int partition(vector<int>& container, int left, int right){
 		}
 		// Move right pointer backwards as long as more than pivot
 		while(container[right] > pivot){
-			right++;
+			right--;
 		}
 		// If left and right pointers have not crossed then swap and
 		// move pointers
@@ -95,13 +114,50 @@ int partition(vector<int>& container, int left, int right){
 
 void quickSort(vector<int>& container, int left, int right){
 	int index = partition(container, left, right);
+	//	sort left part of the container while partition is 
 	if(left < index - 1){
 		quickSort(container, left, index - 1);
 	}
-	else{
+	// 
+	if(index < right){
 		quickSort(container, index, right);
 	}
 }
+
+//	Heap Sort ------------------------------------------------------------------
+
+void heapify(vector<int>& container, int length, int index){
+	int largest = index;
+	int left = 2*index + 1;	// left child
+	int right = 2*index + 2; // right child
+
+	if(left < length && container[left] > container[largest]){
+		largest = left;
+	}
+	if(right < length && container[right] > container[largest]){
+		largest = right;
+	}
+	if(largest != index){
+		swap(container[index], container[largest]);
+		heapify(container, length, largest);
+	}
+}
+
+void heapSort(vector<int>& container){
+	int length = container.size();
+	// Make a max heap in place
+	for(int i = length/2 -1; i >= 0; --i){
+		heapify(container, length, i);
+	}
+	// Swap largest from heap and resort tree with heapify
+	for(int i = length-1; i >= 0; --i){
+		swap(container[i], container[0]);
+		// heapify reduced heap
+		heapify(container, i, 0);
+	}
+}
+
+//	Testing --------------------------------------------------------------------
 
 void printContainer(vector<int>& container){
 	for(auto& c : container){
@@ -116,7 +172,8 @@ void selectSort(vector<int>& container){
 	"2. Insert Sort\n"
 	"3. Merge Sort \n"
 	"4. Quick Sort\n"
-	"5. Exit\n\n";
+	"5. Heap Sort\n"
+	"6. Exit\n\n";
 
 	clock_t start, end;
 	
@@ -149,7 +206,13 @@ void selectSort(vector<int>& container){
 			end = clock();
 			break;
 		}
-		case 5:		exit(0);
+		case 5:{
+			start = clock();
+			heapSort(container);
+			end = clock();
+			break;
+		}
+		case 6:		exit(0);
 		default:	cout << "Invalid choice\n Try Again\n" << endl;
 					selectSort(container);
 					break;	
@@ -166,6 +229,25 @@ void generateContainer(vector<int>& container, int size){
 	for(auto& n : container){
 		n = rand() % 1000 - 500;
 	}
+}
+
+void checkSorting(vector<int>& container, vector<int>& original){
+	if(original.size() != container.size()){
+		cout << "sorting algorithm has changed the number of elements\n" << endl;
+		exit(1);
+	}
+	sort(original.begin(), original.end());
+	int length = original.size();
+	for(int i = 0; i < length; ++i){
+		if(container[i] != original[i]){
+			cout << "Sorting algorithm has an incorrect element - index matchup at"
+				"index " << i << "where the sorted value should be " << original[i]
+				<< " instead of " << container[i] << endl << endl;
+				break;
+		}
+	}
+	cout << "Sorting algorithm sorted container in ascending order and is correct." 
+		<< endl << endl;
 }
 
 int main(int argc, char* argv[]){
@@ -190,10 +272,14 @@ int main(int argc, char* argv[]){
 	vector<int> container(size);
 
 	generateContainer(container, size);
+	// make copy of generated container
+	vector<int> original(container);
 
 	printContainer(container);
-
+	// Sort container
 	selectSort(container);
+	// Check if container is sorted correctly and contains same elements
+	checkSorting(container, original);
 
 	
 	return 0;
