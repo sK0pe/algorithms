cc9600cd408b90fb45617752c28132bbe448a7e0
sK0pe <pradyumn.vij@gmail.com>
2016-08-14 17:03:48 +0800

    More readable code

--- string/strings.cpp
+++ string/strings.cpp
@@ -9,16 +9,20 @@ using namespace std;
 //	Naive-----------------------------------------------------------------------
 void naive(string& pattern, string& text, vector<int>& matches){
 	int pLen = pattern.length();
-	int shifts = text.length() - pLen + 1;
-	bool match;
-	for(int s = 0; s < shifts; ++s){
-		match = true;
-		for(int j = 0; j < pLen; ++j){
-			if(text[s + j] != pattern[j]){
-				match = false;
+	int shifts = text.length() - pLen;
+	// Loop through all possible shifts of window size pattern.length() in text
+	for(int s = 0; s <= shifts; ++s){
+		int i = 0;
+		// Check if each window == pattern
+		for(i = 0; i < pLen; ++i){
+			if(text[s + i] != pattern[i]){
+				// If a character doesn't match then stop checking
+				break;
 			}
 		}
-		if(match){
+		// if loop did not stop checking prematurely, add the index of the
+		// window to the result
+		if(i == pLen){
 			matches.push_back(s);
 		}
 	}
@@ -27,15 +31,58 @@ void naive(string& pattern, string& text, vector<int>& matches){
 //	rabinKarp-------------------------------------------------------------------
 //  assumes pattern is a word that can be compared in constant time
 void rabinKarp(string& pattern, string& text, vector<int>& matches){
-	int num = 0;
-	// big prime
-	int q = 105943;
+	int bigPrime = 105943;
 	int pLen = pattern.length();
+	int shifts = text.length() - pLen;
+
+	int hashPattern = 0;
+	int hashText = 0;
+	int digit = 1;
+	int dictionarySize = 256;	// ascii characters
+
+	// pow(digit, pLen - 1) % bigPrime
+	for(int i = 0; i < pLen-1; ++i){
+		digit = (digit * dictionarySize) % bigPrime;
+	}
+	cout << "digit = " << digit << endl;
+
+	// Find hashed values of pattern and first window in text
 	for(int i = 0; i < pLen; ++i){
+		hashPattern = (dictionarySize * hashPattern + pattern[i]) % bigPrime;
+		hashText = (dictionarySize * hashText + text[i]) % bigPrime;
+		cout << "hashPattern preprocess = " << hashPattern << endl;
+		cout << "hashText preprocess = " << hashText << endl;
+	}
+
+	// Shift window over text
+	for(int s = 0; s <= shifts; ++s){
+		// Check if hash is equivalent
+		if(hashText == hashPattern){
+			int j = 0;
+			// Double check that non spurious
+			for(j = 0; j < pLen; ++j){
+				if(text[s+j] != pattern[j]){
+					break;
+				}
+			}
+			if(j == pLen){
+				matches.push_back(s);
+			}
+		}
+	// Calculate next hashed window, may be negative so get abs
+		if(s < shifts){
+			hashText = abs((dictionarySize * (hashText - text[s]*digit) + 
+				text[s + pLen]) % bigPrime);
+		}
+		cout << "hashText refresh is = " << hashText << endl;
+	}
+	
+
+	/*for(int i = 0; i < pLen; ++i){
 		num = num * 10 + (pattern[i] - '0');
 		//cout << "num is " << num << endl;
 	}
-	num = num % q;
+	num = num % bigPrime;
 	//cout << "num is " << num << endl;
 	// alternate is num = stoi(pattern);
 	int part = 0;
@@ -43,31 +90,96 @@ void rabinKarp(string& pattern, string& text, vector<int>& matches){
 		part = part * 10 + (text[i] - '0');
 	}
 
-	int shifts = text.length() - pLen + 1;
+	int shifts = tLen - pLen + 1;
 
 	for(int s = 0; s < shifts; ++s){
 		// Up date the word interpeted as a number
 		part = (part % int(pow(10, pLen -1))) * 10 + (text[s + pLen - 1] - '0');
-		if(part%q == num){
+		if(part%bigPrime == num){
 			matches.push_back(s);
+		}
+	}*/
+}
+
+// Knuth-Morris-Pratt-----------------------------------------------------------
+void getProperSuffixes(string& pattern, vector<int>& suffixes){
+	int len = 0;
+	suffixes[0] = 0;
+	int pLen = pattern.length();
+	
+	int i = 1;
+	while(i < pLen){
+		if(pattern[i] == pattern[len]){
+			suffixes[i] = ++len;
+			++i;
+		}
+		// pattern[i] != pattern[len]
+		else{
+			if(len != 0){
+				len = suffixes[len-1];
+				// don't increment i
+			}
+			else{	// if len == 0
+				suffixes[i] = 0;
+				++i;
+			}
 		}
 	}
 }
 
-// Knuth-Morris-Pratt-----------------------------------------------------------
 void kmp(string& pattern, string& text, vector<int>& matches){
 	int pLen = pattern.length();
+	int tLen = text.length();
+
+	vector<int> properSuffixes(pLen);
+	getProperSuffixes(pattern, properSuffixes);
+
+	int i = 0;
+	int j = 0;
+	while(i < tLen){
+		// If characters the same move forward
+		if(pattern[j] == text[i]){
+			++j;
+			++i;
+		}
+		// If at the end of pattern length, have found the index at i -j
+		if(j == pLen){
+			matches.push_back(i - j);
+			// Move j back to properSuffixes -1 to check if next character also
+			// matches word
+			j = properSuffixes[j-1];
+		}
+		// mismatch after j matches
+		else if(i < tLen && pattern[j] != text[i]){
+			// skip matching properSuffixes 0 .. propersuffixes[j-1]
+			if(j != 0){
+				// go back to previous position in array
+				j = properSuffixes[j-1];
+			}
+			else{
+				++i;
+			}
+		}
+	}
+
+
+	/*int pLen = pattern.length();
 	vector<int> prefix(pLen + 1, 0);
 	prefix[0] = -1;
 	for(int i = 0; i < pLen; ++i){
 		int index = prefix[i];
-		cout << "index is " << prefix[i] << endl;
+		cout << "i is " << i <<  " and index is " << index << endl;
+		cout << "pattern[index] = " << pattern[index] << " and pattern[i] =  " << pattern[i] << endl;
 		while(index > -1 && pattern[index] != pattern[i]){
 			index = prefix[index];
-			cout << "in while loop index is " << index << " and pattern[index] is "<< pattern[index] << " while pattern[i] is " << pattern[i] << endl; 
+			cout << "pattern[index] = " << pattern[index] << " and pattern[i] =  " << pattern[i] << endl;
+			cout << "index is now prefix[index] = " << index << endl;
+			//cout << "in while loop index is " << index << " and pattern[index] is "<< pattern[index] << " while pattern[i] is " << pattern[i] << endl; 
 		}
 		prefix[i+1] = ++index;
-		cout << "prefix[i+1] is " << index << endl;
+		//cout << "prefix[i+1] is " << index << endl;
+		cout << "i = " << i << " index = " << index << endl;
+		cout << "prefix[i] = " << prefix[i] << " prefix[i + 1] = " << prefix[i+1] << endl << endl;
 	}
 	for(auto& a : prefix){
 		cout << a << ' ';
@@ -84,7 +196,7 @@ void kmp(string& pattern, string& text, vector<int>& matches){
 			matches.push_back(i - pLen + 1);
 			seen = prefix[pLen];
 		}
-	}
+	}*/
 }
 
 
