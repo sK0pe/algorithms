421d2f1b985a356bac6c4e894a6f3aa974c4138a
sK0pe <pradyumn.vij@gmail.com>
2016-08-30 22:57:55 +0800

    Sort of understand the insertion bit need to implment

--- tsp/coalescedTSP.cpp
+++ tsp/coalescedTSP.cpp
@@ -15,46 +15,73 @@ class edge{
 		}
 };
 
-class subset{
-	public:
-		int parent;
-		int rank;
-};
+// class subset{
+// 	public:
+// 		int parent;
+// 		int rank;
+// };
 
-// finds the set of an element i
-int findSubset(vector<subset>& subs, int i){
-	// find the root and make root parent of i
-	if(subs[i].parent != i){
-		subs[i].parent = findSubset(subs, subs[i].parent);
+// // finds the set of an element i
+// int findSubset(vector<subset>& subs, int i){
+// 	// find the root and make root parent of i
+// 	if(subs[i].parent != i){
+// 		subs[i].parent = findSubset(subs, subs[i].parent);
+// 	}
+// 	return subs[i].parent;
+// }
+
+// void unify(vector<subset>& subs, int x, int y){
+// 	int xroot = findSubset(subs, x);
+// 	int yroot = findSubset(subs, y);
+
+// 	// Add smaller rank tree to higher rank treets
+// 	if(subs[xroot].rank > subs[yroot].rank){
+// 		subs[yroot].parent = xroot;
+// 	}
+// 	else if(subs[xroot].rank < subs[yroot].rank){
+// 		subs[xroot].parent = yroot;
+// 	}
+// 	else{
+// 		subs[yroot].parent = xroot;
+// 		subs[xroot].rank++;
+// 	}
+
+// }
+
+int root(int i, vector<int>& parent){
+	while(i != parent[i]){
+		// make every node that isn't already it's own root, point
+		// to the grandparent
+		parent[i] = parent[parent[i]];
+		i = parent[i];
 	}
-	return subs[i].parent;
+	return i;
 }
 
-void unify(vector<subset>& subs, int x, int y){
-	int xroot = findSubset(subs, x);
-	int yroot = findSubset(subs, y);
+bool inSameSet(int p, int q, vector<int>&parent){
+	return root(p, parent) == root(q, parent);
+}
 
-	// Add smaller rank tree to higher rank treets
-	if(subs[xroot].rank > subs[yroot].rank){
-		subs[yroot].parent = xroot;
-	}
-	else if(subs[xroot].rank < subs[yroot].rank){
-		subs[xroot].parent = yroot;
+void unite(int p, int q, vector<int>& parent, vector<int>& elements){
+	int i = root(p, parent);
+	int j = root(q, parent);
+	if(elements[i] < elements[j]){
+		parent[i] = j;
+		elements[j] += parent[i];
 	}
 	else{
-		subs[yroot].parent = xroot;
-		subs[xroot].rank++;
+		parent[j] = i;
+		elements[i] += elements[j];
 	}
-
 }
 
-
 void kruskals(int numNodes, vector<edge>& grid, vector<edge>& minPath){
-	vector<subset> subs(numNodes);
+	vector<int> parent(numNodes);
+	vector<int> elements(numNodes);
+	//vector<subset> subs(numNodes);
 	vector<int> degree(numNodes, 0);
 	for(int v = 0; v < numNodes; ++v){
-		subs[v].parent = v;
-		subs[v].rank = 0;
+		parent[v] = v;
 	}
 
 	// Sort edges by weight in ascending order
@@ -67,25 +94,41 @@ void kruskals(int numNodes, vector<edge>& grid, vector<edge>& minPath){
 		edge currCheapest = grid[i];
 		i++;	// go to next edge in sorted edges
 
-		int x = findSubset(subs, currCheapest.source);
-		int y = findSubset(subs, currCheapest.destination);
-
 		// Checking degrees for coalesced simple paths
-		if(x != y && degree[currCheapest.source] != 2 && degree[currCheapest.destination] != 2){
+		// On top of kruskals checking if 2 points are in the same set by checking roots
+		if(!inSameSet(currCheapest.source, currCheapest.destination, parent) 
+			&& degree[currCheapest.source] != 2 && degree[currCheapest.destination] != 2){
+			
 			minPath[e] = currCheapest;
 			e++;	// go to next edge in MST
-			unify(subs, x, y);
+			unite(currCheapest.source, currCheapest.destination, parent, elements);
 			degree[currCheapest.source]++;
 			degree[currCheapest.destination]++;
 		}
 	}
 
-	// Remove random 0, 0
+	//Remove random 0, 0
 	for(int i = 0; i < numNodes; ++i){
 		if(minPath[i].source == minPath[i].destination){
 			minPath.erase(minPath.begin() + i);
+			degree[i]--;
 		}
 	}
+
+	// Insertion???
+	for(int i = 0; i < numNodes; ++i){
+		if(degree[i] == 0){
+			int x = degree[i];
+			// Check each edge that is inside of minPath, add the unused points
+			edge minEdge;
+			for(int e = 0; e < (int)minPath.size(); ++e){
+				// how to determine distance?
+
+			}
+		}
+	}
+
+
 }
 
 void tsp(int numNodes, int numEdges, vector<edge>& grid){
